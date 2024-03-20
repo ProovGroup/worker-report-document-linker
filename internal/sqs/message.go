daf8a2e12c5fdb1e94319af4b87c41726b69a50b
Simon Lasbrugnas <simon.ext@weproov.com>
2024-03-20 11:49:57 +0100

    Change message to S3 event

--- internal/sqs/message.go
+++
@@ -1,9 +0,0 @@
-package sqs
-
-import "worker-report-document-linker/internal/document"
-
-type Message struct {
-	ProovCode string            `json:"proov_code"`
-	Document  document.Document `json:"document"`
-}
-
--- pkg/lambda/handler.go
+++ pkg/lambda/handler.go
@@ -5,44 +5,71 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 	"worker-report-document-linker/internal/db"
-	"worker-report-document-linker/internal/sqs"
+	"worker-report-document-linker/internal/document"
 
 	"github.com/ProovGroup/env"
 	"github.com/aws/aws-lambda-go/events"
 )
 
 func Handler(ctx context.Context, event events.SQSEvent) error {
-	for i := range event.Records {
+	for i, sqsRecord := range event.Records {
 		e, err := env.GetEnvSqsArnSSM(event.Records[i].EventSourceARN, os.Getenv("AWS_REGION"), env.BDDWrite)
 		if err != nil {
 			fmt.Println("[ERROR] env.GetEnvSqsArnSSM:", err)
 			return err
 		}
 
-		var message sqs.Message
-		if err = json.Unmarshal([]byte(event.Records[i].Body), &message); err != nil {
-			fmt.Println("[ERROR] json.Unmarshal:", err)
-			return err
-		}
+		s3Event := events.S3Event{}
 
-		fmt.Println("[INFO] ProovCode:", message.ProovCode)
-		fmt.Println("[INFO] Document:", message.Document)
-
-		// Get permalink
-		link := message.Document.GetPermalink()
-		if link == "" {
-			return fmt.Errorf("[ERROR] link is empty")
-		}
-
-		// Save document
-		err = db.SaveDocument(&e, message.ProovCode, &message.Document, link)
+		err = json.Unmarshal([]byte(sqsRecord.Body), &s3Event)
 		if err != nil {
-			fmt.Println("[ERROR] db.SaveDocument:", err)
+			fmt.Println("[Error] Unmarshal record event", err)
 			return err
 		}
 
-		fmt.Println("[INFO] Document linked successfully")
+		for _, s3Record := range s3Event.Records {
+			s3Region := s3Record.AWSRegion
+			s3Bucket := s3Record.S3.Bucket.Name
+			s3Key := s3Record.S3.Object.Key
+
+			parts := strings.Split(s3Key, "/")
+			if len(parts) < 2 {
+				return fmt.Errorf("[ERROR] Invalid key %s. The key must be in this format [...]/{proov_code}/{file}.{ext}.\n", s3Key)
+			}
+
+			proovCode := parts[len(parts)-2]
+			file := strings.Split(parts[len(parts)-1], ".")
+
+			document := document.Document {
+				Name: file[0],
+				Type: file[1],
+				Path: document.Path {
+					Region: s3Region,
+					Bucket: s3Bucket,
+					Key:    s3Key,
+				},
+			}
+
+			fmt.Println("[INFO] ProovCode:", proovCode)
+			fmt.Println("[INFO] Document:", document)
+
+			// Get permalink
+			link := document.GetPermalink()
+			if link == "" {
+				return fmt.Errorf("[ERROR] link is empty")
+			}
+
+			// Save document
+			err = db.SaveDocument(&e, proovCode, &document, link)
+			if err != nil {
+				fmt.Println("[ERROR] db.SaveDocument:", err)
+				return err
+			}
+
+			fmt.Println("[INFO] Document linked successfully")
+		}
 	}
 
 	return nil
