d021f62c43c2ccc26cdb34eaead9d499cf3ac9e3
Simon Lasbrugnas <simon.ext@weproov.com>
2024-04-03 14:43:04 +0200

    Make permalink's base url configurable in aws env

--- internal/permalink/permalink.go
+++ internal/permalink/permalink.go
@@ -7,17 +7,19 @@ import (
 	permalink "github.com/ProovGroup/lib-permalink"
 )
 
-var ENV = os.Getenv("ENV")
+var (
+	ENV = os.Getenv("ENV")
+	BASE_URL = os.Getenv("BASE_URL")
+)
 
 func GetPermalink(region string, bucket string, key string) string {
 	if (key == "") {
 		return ""
 	}
 
-	baseURL := "https://permalink2.weproov.com"
 	link := permalink.NewPermalink(ENV, permalink.S3, 0)
 
-	url, err := link.SetRegion(region).SetBucket(bucket).SetKey(key).AppendToURL(baseURL)
+	url, err := link.SetRegion(region).SetBucket(bucket).SetKey(key).AppendToURL(BASE_URL)
 	if err != nil {
 		fmt.Println("[ERROR] GetPermalink:", err)
 		return ""
