c1507627d599a24c4680061e180e4d97ba8f8643
Simon Lasbrugnas <simon.ext@weproov.com>
2024-05-17 10:20:13 +0200

    Fix missing webhooks notifier config file

--- internal/provider/env.go
+++ internal/provider/env.go
@@ -14,6 +14,7 @@ func init() {
 	bucketEnvName := "weproov-environment-" + os.Getenv("ENV") + "-files"
 	files := []string{
 		"s3://" + bucketEnvName + "/V1/env_core_main_database.json:eu-west-1",
+		"s3://" + bucketEnvName + "/V1/env_sqs_webhooks_notifier.json:eu-west-1",
 	}
 
 	ev, err := env.NewEnvironment(strings.Join(files, ","))
