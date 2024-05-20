26d9697dff0828cc03f54babb7320eedbc3f356d
Simon Lasbrugnas <simon.ext@weproov.com>
2024-05-20 13:57:38 +0200

    Add document existence check before saving

--- internal/document/repository.go
+++ internal/document/repository.go
@@ -1,6 +1,7 @@
 package document
 
 import (
+	"fmt"
 	"worker-report-document-linker/internal/provider"
 
 	"github.com/ProovGroup/lib-env/database"
@@ -19,9 +20,19 @@ func NewRepository(doc *Document) *Repository {
 }
 
 func (r *Repository) Save(proovCode string, link string) error {
+	var count int
+	r.db.QueryRow(`SELECT COUNT(1) FROM rptdocuments WHERE proov_code = $1 AND url = $2`, proovCode, link).Scan(&count)
+	if count > 0 {
+		fmt.Println("[WARN] This document has already been linked to this proov_code", proovCode, link)
+		return nil
+	}
 	_, err := r.db.Exec(
 		`INSERT INTO rptdocuments (name, type, proov_code, url) VALUES ($1, $2, $3, $4)`,
 		r.Document.Name, r.Document.Type, proovCode, link,
 	)
-	return err
+	if err != nil {
+		return err
+	}
+	fmt.Println("[INFO] Document linked successfully")
+	return nil
 }
